c1ff67fcbe49b62e86a9362837f04a89a1e83f16
Dany <44971442+Skardyone@users.noreply.github.com>
2023-03-06 20:18:06 +0100

    title mobile done

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Logo from "../../assets/design/decode_final_bile.png"
 import Kapela from "../../assets/design/kapela_vyrezy.png"
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Title = () => {
   return (
@@ -13,11 +14,11 @@ const Title = () => {
             <Image alt='kapela' src={Kapela}/>
         </div>
         <div className="onas bg-[#50A983] w-[80%] p-4 text-[1.5rem] text-white rounded-md text-center mb-8">
-            <button className='uppercase'>o nás</button>
+            <Link href="/"><button className='uppercase'>o nás</button></Link>
         </div>
         <div className='flex flex-row justify-evenly w-full gap-2 text-white'>
-            <button className="kontakt uppercase border-2 border-[#50A983] p-3 text-[1rem] basis-1/2 rounded-md">kontakty</button>
-            <button className="poradatele uppercase border-2 border-[#50A983] p-3 text-[1rem] basis-1/2 rounded-md">pro pořadatele</button>
+            <Link href="/"><button className="kontakt uppercase border-2 border-[#50A983] py-1 px-2 text-[1rem] basis-1/2 rounded-md">kontakty</button></Link>
+            <Link href="/"><button className="poradatele uppercase border-2 border-[#50A983] py-1 px-2 text-[1rem] basis-1/2 rounded-md">pro pořadatele</button></Link>
         </div>
     </div>
   )
