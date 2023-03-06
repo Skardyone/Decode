1d01c2e66a7733821b7823b25e4d48cbdcdd15a5
Dany <44971442+Skardyone@users.noreply.github.com>
2023-03-06 20:12:29 +0100

    title mobile

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -3,9 +3,9 @@ import { FaYoutube, FaInstagram, FaFacebookSquare } from "react-icons/fa"
 
 const Socials = () => {
   return (
-    <div className='fixed flex flex-row bottom-6 left-6 w-auto p-1 rounded-md text-[2.5rem] text-white bg-white/5'>
+    <div className='fixed flex flex-col items-center bottom-6 left-3 w-auto p-1 rounded-md text-[2rem] text-white bg-white/5'>
         <div><FaFacebookSquare/></div>
-        <div className='px-2'><FaInstagram/></div>
+        <div className='py-1'><FaInstagram/></div>
         <div ><FaYoutube/></div>
     </div>
   )
