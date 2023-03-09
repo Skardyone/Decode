b761c89d8458596103f1010e23bdd9488c91aad2
Dany <44971442+Skardyone@users.noreply.github.com>
2023-03-09 14:38:03 +0100

    Head

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,19 @@ import Sticky from '@/components/Sticky'
 import Koncerty from '@/components/Koncerty'
 import Player from '@/components/Player'
 import Divider from '@/components/Divider'
+import Head from "next/head"
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main>
+      <head>
+        <title>DECODE</title>
+        <meta name="description" content='Wepage kapely Decode'/>
+        <meta name='viewport' content='width=device-width, initial-scale=1'/>
+        <link rel="icon" href='/decode_final_bile.png'/>
+      </head>
       <div className='bg-[#212139]'>
         <Socials/>
         <Sticky/>
