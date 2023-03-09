449012dcbfb0b5cb4d6da2ee438f2d52759e1988
Dany <44971442+Skardyone@users.noreply.github.com>
2023-03-09 14:24:19 +0100

    facebook + bandzone

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,11 +1,12 @@
 import Link from 'next/link'
 import React from 'react'
-import { FaYoutube, FaInstagram, FaFacebookSquare } from "react-icons/fa"
+import { FaYoutube, FaInstagram, FaFacebookSquare, FaDrum } from "react-icons/fa"
 
 const Socials = () => {
   return (
     <div className='fixed flex z-30 flex-col items-center bottom-6 left-3 w-auto p-1 rounded-md text-[2rem] text-white bg-white/5'>
-        <Link href="https://www.facebook.com/people/Decode/" target={"_blank"}><FaFacebookSquare/></Link>
+        <Link href="https://www.facebook.com/people/Decode/100040249697899/" target={"_blank"}><FaFacebookSquare/></Link>
+        <Link href="https://bandzone.cz/decodemusic" target={"_blank"}><FaDrum/></Link>
         <Link href="https://www.instagram.com/decode_officialband/" className='py-1' target={"_blank"}><FaInstagram/></Link>
         <Link href="https://www.youtube.com/channel/UCrAWaFd3XByZW-Iw6Ymu6zw" target={"_blank"}><FaYoutube/></Link>
     </div>
